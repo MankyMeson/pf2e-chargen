b49a5621de8ff94dc60bdc6a98e46e3195618084
Clio <cliokj22@gmail.com>
2025-11-17 14:19:18 +0000

    other die sizes

--- pf2echargen/lib/random.py
+++ pf2echargen/lib/random.py
@@ -8,3 +8,18 @@ def d20():
     """
     """
     return random.randrange(1, 21)
+
+def d10():
+    return random.randrange(1, 11)
+
+def d8():
+    return random.randrange(1, 9)
+
+def d6():
+    return random.randrange(1, 7)
+
+def d4():
+    return random.randrange(1, 5)
+
+def d3():
+    return random.randrange(1, 4)
